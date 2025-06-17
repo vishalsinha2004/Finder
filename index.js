2727adc61eb72df7a037f84022356d59e6168b7b
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-06-17 12:22:24 +0530

    Change UI

--- index.js
+++ index.js
@@ -2,41 +2,55 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
+const marked = require('marked'); // Import marked library
 
+// Configure marked
+marked.setOptions({
+  breaks: true,
+  gfm: true
+});
+
+// Set up Express
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', function(req, res){
-    fs.readdir(`./files`,function(err, files){
-        res.render("index", {files: files});
-    })
-   
-})
+// Make marked available to all templates
+app.locals.marked = marked;
 
-app.get('/file/:filename', function(req, res){
-    fs.readFile(`./files/${req.params.filename}` ,"utf-8",function(err, filedata){
-        res.render('show', {filename: req.params.filename, filedata: filedata});
-    })
-   
-})
-
-app.get('/edit/:filename', function(req, res){
-   res.render('edit', {filename: req.params.filename});
-   
-})
-app.post('/edit', function(req, res){
-        fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err){
-            res.redirect("/");
-        })
-
-})
-
-app.post('/create', function(req, res){
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
-        res.redirect("/")
+// Routes
+app.get('/', function(req, res) {
+    fs.readdir(`./files`, function(err, files) {
+        res.render("index", { files: files });
     });
-})
+});
 
-app.listen(3000);
+app.get('/file/:filename', function(req, res) {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata) {
+        res.render('show', {
+            filename: req.params.filename,
+            filedata: filedata
+        });
+    });
+});
+
+app.get('/edit/:filename', function(req, res) {
+    res.render('edit', { filename: req.params.filename });
+});
+
+app.post('/edit', function(req, res) {
+    fs.rename(`./files/${req.body.previous}`, `./files/${req.body.new}`, function(err) {
+        res.redirect("/");
+    });
+});
+
+app.post('/create', function(req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err) {
+        res.redirect("/");
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
