5f0aad9ff90f2c3965172bf1504f0738c346c0ab
vishalsinha2004 <vishalsinha6567@gmail.com>
2025-07-04 16:07:51 +0530

    refactor: use environment variable for server port configuration

--- index.js
+++ index.js
@@ -100,6 +100,8 @@ app.post('/create', function(req, res) {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000'); 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
